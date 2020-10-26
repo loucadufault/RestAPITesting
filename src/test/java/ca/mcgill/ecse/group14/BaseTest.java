dd2f121b1e03f6c158c8496a5ead4a09108e15f8
Louca Dufault <louca.dufault@gmail.com>
2020-10-25 23:23:38 -0400

    sample test works

--- src/test/java/ca/mcgill/ecse/group14/BaseTest.java
+++ src/test/java/ca/mcgill/ecse/group14/BaseTest.java
@@ -1,4 +1,18 @@
 package ca.mcgill.ecse.group14;
 
+import org.junit.Test;
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.*;
+
 public class BaseTest {
+    @Test
+    public void test_NumberOfCircuitsFor2017Season_ShouldBe20() {
+
+        given().
+                when().
+                get("http://ergast.com/api/f1/2017/circuits.json").
+                then().
+                assertThat().
+                body("MRData.CircuitTable.Circuits.circuitId",hasSize(20));
+    }
 }
