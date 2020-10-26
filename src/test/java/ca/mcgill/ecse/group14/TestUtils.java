0d577e8e554cd4cba9eb085c0ce354f1d13f9f83
Caleb Lim <caleb.lim@mail.mcgill.ca>
2020-10-26 18:35:10 -0400

    added helper functions

--- src/test/java/ca/mcgill/ecse/group14/TestUtils.java
+++ src/test/java/ca/mcgill/ecse/group14/TestUtils.java
@@ -1,6 +1,11 @@
 package ca.mcgill.ecse.group14;
 
+import static ca.mcgill.ecse.group14.Resources.*;
+
+import io.restassured.RestAssured;
 import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+import org.json.simple.JSONObject;
 
 import static io.restassured.RestAssured.*;
 import static org.junit.Assert.assertEquals;
@@ -16,5 +21,38 @@ public class TestUtils {
 
     public static void assertHEADStatusCode(String url, int statusCode) {
         assertResponseStatusCode(head(url), statusCode);
+    }
+
+    public static int createTodoHelper(String title) throws Exception {
+        JSONObject fields = new JSONObject();
+        fields.put("title", title);
+
+        RequestSpecification request = RestAssured.given();
+        request.body(fields.toJSONString());
+
+        Response response = request.post(BASE_URL+"/todos");
+        return Integer.parseInt((String) response.jsonPath().get("id"));
+    }
+
+    public static int createProjectHelper(String title) throws Exception {
+        JSONObject fields = new JSONObject();
+        fields.put("title", title);
+
+        RequestSpecification request = RestAssured.given();
+        request.body(fields.toJSONString());
+
+        Response response = request.post(BASE_URL+"/projects");
+        return Integer.parseInt((String) response.jsonPath().get("id"));
+    }
+
+    public static int createCategoryHelper(String title) throws Exception {
+        JSONObject fields = new JSONObject();
+        fields.put("title", title);
+
+        RequestSpecification request = RestAssured.given();
+        request.body(fields.toJSONString());
+
+        Response response = request.post(BASE_URL+"/categories");
+        return Integer.parseInt((String) response.jsonPath().get("id"));
     }
 }
