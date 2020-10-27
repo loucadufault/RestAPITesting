03941441f4385186454c8bae6db3473a357b6166
Ryan Arndtsen <arndtsenr@gmail.com>
2020-10-26 23:34:56 -0400

    Finished Docs Tests

---
+++ src/test/java/ca/mcgill/ecse/group14/DocsTest.java
@@ -0,0 +1,71 @@
+package ca.mcgill.ecse.group14;
+
+import io.restassured.http.ContentType;
+import org.junit.*;
+
+import static ca.mcgill.ecse.group14.Resources.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import io.restassured.path.xml.XmlPath;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+import static io.restassured.path.xml.XmlPath.CompatibilityMode.HTML;
+
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.core.IsEqual.equalTo;
+
+
+
+public class DocsTest {
+    private static final String EXPECTED = "API Documentation";
+
+    @Before
+    public void setup() {
+        Server.start();
+        Server.check();
+    }
+
+    @After
+    public void teardown() throws InterruptedException {
+        Server.stop();
+        Thread.sleep(500);
+    }
+
+    /*
+    Test /docs loads the documentation
+     */
+    @Test
+    public void testDocumentationLoaded(){
+        given()
+                .header("Content-Type", "application/json")
+                .header("Accept", "application/json")
+                .get(BASE_URL+"/docs")
+                .then().assertThat()
+                .statusCode(STATUS_CODE.OK);
+    }
+
+    /*
+    Test that the body of the documentation is HTML
+     */
+    @Test
+    public void testDocumentationHTML(){
+        given()
+                .header("Content-Type", "application/json")
+                .header("Accept", "application/json");
+        Response r = given()
+                .header("Content-Type", "application/json")
+                .header("Accept", "application/json")
+                .get(BASE_URL+"/docs")
+                .then()
+                .contentType(ContentType.HTML)
+                .extract()
+                .response();
+        XmlPath htmlpath = new XmlPath(HTML, r.getBody().asString());
+        Assert.assertEquals(htmlpath.getString("html.head.title"), EXPECTED);
+    }
+
+}
