7863d4a1e6ac23ecfbbf0756015785ea08c57f8e
Caleb Lim <caleb.lim@mail.mcgill.ca>
2020-10-27 00:16:59 -0400

    updated test

--- src/test/java/ca/mcgill/ecse/group14/TodosTest.java
+++ src/test/java/ca/mcgill/ecse/group14/TodosTest.java
@@ -124,23 +124,6 @@ public class TodosTest {
     }
 
     /**
-     * Test POST http://localhost:4567/todos using malformed JSON payload
-     */
-    @Test
-    public void testPostTodosUsingMalformedJSON() {
-        JSONObject fields = new JSONObject();
-        fields.put("!text123", "titleText");
-
-        given()
-                .header("Content-Type", "application/json")
-                .header("Accept", "application/json")
-                .body(fields.toJSONString())
-                .post(BASE_URL + "/todos")
-                .then().assertThat()
-                .statusCode(STATUS_CODE.BAD_REQUEST);
-    }
-
-    /**
      * Test POST http://localhost:4567/todos using XML payload
      */
     @Test
