81fa0e2f841d74b657c010faa6f58feb042865cb
Ryan Arndtsen <arndtsenr@gmail.com>
2020-10-27 01:26:08 -0400

    Added all classes to suite

--- src/test/java/ca/mcgill/ecse/group14/TestRunner.java
+++ src/test/java/ca/mcgill/ecse/group14/TestRunner.java
@@ -6,7 +6,7 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
 @RunWith(Suite.class)
-@Suite.SuiteClasses({BaseTest.class, TodosTest.class, ProjectsTest.class})
+@Suite.SuiteClasses({BaseTest.class, TodosTest.class, ProjectsTest.class, CategoriesTest.class, DocsTest.class, ShutDownTest.class})
 public class TestRunner {
     @BeforeClass
     public static void setup() {
