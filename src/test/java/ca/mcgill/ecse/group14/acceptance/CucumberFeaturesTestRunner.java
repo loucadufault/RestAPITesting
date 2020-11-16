8dd0717efcfa5496aae52b0f3511d7732d4acaec
Louca Dufault <louca.dufault@gmail.com>
2020-11-16 17:38:33 -0500

    add cucumber test runner and dep

---
+++ src/test/java/ca/mcgill/ecse/group14/acceptance/CucumberFeaturesTestRunner.java
@@ -0,0 +1,10 @@
+package ca.mcgill.ecse.group14.acceptance;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(plugin = "pretty", features = "src/test/resources", glue = "ca.mcgill.ecse.group14.acceptance")
+public class CucumberFeaturesTestRunner {
+}
