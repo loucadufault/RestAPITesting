2047c7e503cb06c551dc6afeb7f95ab9c7bd83d5
Ryan Arndtsen <arndtsenr@gmail.com>
2020-11-16 23:26:44 -0500

    idk anymore

--- src/test/java/ca/mcgill/ecse/group14/acceptance/CategorizeTaskPriorityStepDefinitions.java
+++ src/test/java/ca/mcgill/ecse/group14/acceptance/CategorizeTaskPriorityStepDefinitions.java
@@ -9,51 +9,5 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 public class CategorizeTaskPriorityStepDefinitions {
-    @When("the user attempts to assign priority {string} to todo Requirements")
-    public void the_user_attempts_to_assign_priority_to_todo_requirements(String string) {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
-    }
 
-    @Then("the todo Requirements will be have priority {string}")
-    public void the_todo_requirements_will_be_have_priority(String string) {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
-    }
-
-    @When("the user attempts to assign priority {string} to todo Quality Assurance")
-    public void the_user_attempts_to_assign_priority_to_todo_quality_assurance(String string) {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
-    }
-
-    @Then("the todo Quality Assurance will be have priority {string}")
-    public void the_todo_quality_assurance_will_be_have_priority(String string) {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
-    }
-
-    @Given("the todo Requirements is assigned priority {string}")
-    public void the_todo_requirements_is_assigned_priority(String string) {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
-    }
-
-    @When("the user attempts to assign priority {string} to todo {string}")
-    public void the_user_attempts_to_assign_priority_to_todo(String string, String string2) {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
-    }
-
-    @Then("the priority of todo {string} is updated from {string} to {string}")
-    public void the_priority_of_todo_is_updated_from_to(String string, String string2, String string3) {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
-    }
-
-    @Given("the todo Quality Assurance is assigned priority {string}")
-    public void the_todo_quality_assurance_is_assigned_priority(String string) {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
-    }
 }
--- src/test/java/ca/mcgill/ecse/group14/acceptance/UpdateTaskPriorityStepDefinitions.java
+++ src/test/java/ca/mcgill/ecse/group14/acceptance/UpdateTaskPriorityStepDefinitions.java
@@ -9,51 +9,5 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 public class UpdateTaskPriorityStepDefinitions {
-    @Given("the following todos exist in the system:")
-    public void the_following_todos_exist_in_the_system(io.cucumber.datatable.DataTable dataTable) {
-        // Write code here that turns the phrase above into concrete actions
-        // For automatic transformation, change DataTable to one of
-        // E, List<E>, List<List<E>>, List<Map<K,V>>, Map<K,V> or
-        // Map<K, List<V>>. E,K,V must be a String, Integer, Float,
-        // Double, Byte, Short, Long, BigInteger or BigDecimal.
-        //
-        // For other transformations you can register a DataTableType.
-        throw new io.cucumber.java.PendingException();
-    }
 
-    @Given("there exists a todo with title {string} in the system")
-    public void there_exists_a_todo_with_title_in_the_system(String string) {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
-    }
-
-    @When("the user attempts to update the priority to priority {string} of the todo with title {string}")
-    public void the_user_attempts_to_update_the_priority_to_priority_of_the_todo_with_title(String string, String string2) {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
-    }
-
-    @Then("the todo with title {string} has priority {string}")
-    public void the_todo_with_title_has_priority(String string, String string2) {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
-    }
-
-    @Given("the todo with title {string} is not assigned to a priority")
-    public void the_todo_with_title_is_not_assigned_to_a_priority(String string) {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
-    }
-
-    @Given("there does not exist a todo with title {string} in the system")
-    public void there_does_not_exist_a_todo_with_title_in_the_system(String string) {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
-    }
-
-    @Then("the system shall report the error code {string}")
-    public void the_system_shall_report_the_error_code(String string) {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
-    }
 }
