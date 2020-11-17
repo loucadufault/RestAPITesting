05b9dd10ba5496a7f28e20774e48ed7fc7621f12
Volen <volen.mihaylov@yahoo.com>
2020-11-16 23:41:42 -0500

    Volen True Skelly

--- src/test/java/ca/mcgill/ecse/group14/acceptance/QueryIncompleteHighPriorityTasks.java
+++ src/test/java/ca/mcgill/ecse/group14/acceptance/QueryIncompleteHighPriorityTasks.java
@@ -10,4 +10,76 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 public class QueryIncompleteHighPriorityTasks {
 
+    @Given("the following priorities are in memory")
+    public void the_following_priorities_are_in_memory(io.cucumber.datatable.DataTable dataTable) {
+        // Write code here that turns the phrase above into concrete actions
+        // For automatic transformation, change DataTable to one of
+        // E, List<E>, List<List<E>>, List<Map<K,V>>, Map<K,V> or
+        // Map<K, List<V>>. E,K,V must be a String, Integer, Float,
+        // Double, Byte, Short, Long, BigInteger or BigDecimal.
+        //
+        // For other transformations you can register a DataTableType.
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Given("the following projects are in memory")
+    public void the_following_projects_are_in_memory(io.cucumber.datatable.DataTable dataTable) {
+        // Write code here that turns the phrase above into concrete actions
+        // For automatic transformation, change DataTable to one of
+        // E, List<E>, List<List<E>>, List<Map<K,V>>, Map<K,V> or
+        // Map<K, List<V>>. E,K,V must be a String, Integer, Float,
+        // Double, Byte, Short, Long, BigInteger or BigDecimal.
+        //
+        // For other transformations you can register a DataTableType.
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Given("the following todos are saved under {string}")
+    public void the_following_todos_are_saved_under(String string, io.cucumber.datatable.DataTable dataTable) {
+        // Write code here that turns the phrase above into concrete actions
+        // For automatic transformation, change DataTable to one of
+        // E, List<E>, List<List<E>>, List<Map<K,V>>, Map<K,V> or
+        // Map<K, List<V>>. E,K,V must be a String, Integer, Float,
+        // Double, Byte, Short, Long, BigInteger or BigDecimal.
+        //
+        // For other transformations you can register a DataTableType.
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Given("there exists a project with title {string} in the system")
+    public void there_exists_a_project_with_title_in_the_system(String string) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Given("the project with title {string} has active tasks")
+    public void the_project_with_title_has_active_tasks(String string) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @When("the user attempts to query the incomplete high priority tasks of the project with title {string}")
+    public void the_user_attempts_to_query_the_incomplete_high_priority_tasks_of_the_project_with_title(String string) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("\"\"{int}\"\" todos will be returned")
+    public void todos_will_be_returned(Integer int1) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Given("the project with title {string} is inactive")
+    public void the_project_with_title_is_inactive(String string) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Given("the project with title {string} has no tasks")
+    public void the_project_with_title_has_no_tasks(String string) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
 }
--- src/test/java/ca/mcgill/ecse/group14/acceptance/QueryIncompleteTasks.java
+++ src/test/java/ca/mcgill/ecse/group14/acceptance/QueryIncompleteTasks.java
@@ -9,5 +9,27 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 public class QueryIncompleteTasks {
+    @Given("there exists a project with title <string> in the system")
+    public void there_exists_a_project_with_title_string_in_the_system() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
 
+    @Then("{string} todos will be returned")
+    public void todos_will_be_returned(String string) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Given("the project with title {string} has no active tasks")
+    public void the_project_with_title_has_no_active_tasks(String string) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Given("there does not exist a project with title <string> in the system")
+    public void there_does_not_exist_a_project_with_title_string_in_the_system() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
 }
