31d0b44bdc983376fb5f35722ceb34a37ca36a2d
Louca Dufault <louca.dufault@gmail.com>
2020-11-16 22:36:07 -0500

    refactor feature files

--- src/test/java/ca/mcgill/ecse/group14/acceptance/BaseStepDefinitions.java
+++ src/test/java/ca/mcgill/ecse/group14/acceptance/BaseStepDefinitions.java
@@ -6,7 +6,7 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 public class BaseStepDefinitions {
-    @Given("^the Todo Manager ReST API server is running$")
+    @Given("^the Todo Manager Rest API server is running$")
     public void the_todo_manager_rest_api_server_is_running() {
 
     }
