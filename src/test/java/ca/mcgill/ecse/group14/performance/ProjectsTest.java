d8a618f14277950e52cbd7a3c6d87cf1217a7425
Ryan Arndtsen <arndtsenr@gmail.com>
2020-12-04 01:09:43 -0500

    Made project performance tests

--- src/test/java/ca/mcgill/ecse/group14/performance/ProjectsTest.java
+++ src/test/java/ca/mcgill/ecse/group14/performance/ProjectsTest.java
@@ -1,4 +1,42 @@
 package ca.mcgill.ecse.group14.performance;
 
-public class ProjectsTest {
+import ca.mcgill.ecse.group14.Resources;
+import ca.mcgill.ecse.group14.Utils;
+import ca.mcgill.ecse.group14.acceptance.BaseStepDefinitions;
+import org.junit.Test;
+
+public class ProjectsTest extends BaseTest {
+
+    @Test
+    public void test_AddProjectsWhileIncreasingNumberOfProjects() {
+        clearObjects();
+        for (int i = 0; i< Resources.OBJECT_COUNT_INCREASE_PROGRESSION.length; i++) {
+            Utils.createPopulatedProjects(Resources.OBJECT_COUNT_INCREASE_PROGRESSION[i]);
+            objectCount += Resources.OBJECT_COUNT_INCREASE_PROGRESSION[i];
+            logTransactionTime(Utils.createPopulatedProject());
+            objectCount += 1;
+        }
+    }
+
+    @Test
+    public void test_DeleteProjectWhileIncreasingNumberOfProjects() {
+        clearObjects();
+        for (int i = 0; i< Resources.OBJECT_COUNT_INCREASE_PROGRESSION.length; i++) {
+            Utils.createPopulatedProjects(Resources.OBJECT_COUNT_INCREASE_PROGRESSION[i]);
+            objectCount += Resources.OBJECT_COUNT_INCREASE_PROGRESSION[i];
+            logTransactionTime(Utils.deletePopulatedProject());
+            objectCount -= 1;
+        }
+    }
+
+    @Test
+    public void test_ChangeProjectWhileIncreasingNumberOfProjects() {
+        clearObjects();
+        for (int i = 0; i< Resources.OBJECT_COUNT_INCREASE_PROGRESSION.length; i++) {
+            Utils.createPopulatedProjects(Resources.OBJECT_COUNT_INCREASE_PROGRESSION[i]);
+            objectCount += Resources.OBJECT_COUNT_INCREASE_PROGRESSION[i];
+            logTransactionTime(Utils.changePopulateProject());
+            objectCount += 1;
+        }
+    }
 }
